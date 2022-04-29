09e402c3c562c2ecd993796ad2669d65ad6f8236
Vishal Sharma <sharma.vishal2799@gmail.com>
2022-04-29 07:03:17 +0530

    initial commit

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+
+export {SubInfo,
+    HomeHeader, DetailsBid, DetailsDesc, NFTCard, FocusedStatusBar, CircleButton, RectButton
+}
